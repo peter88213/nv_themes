895155907f8ed5ca658149687353d8da55b72612
peter88213 <petert88213@gmail.com>
2025-08-15 19:05:25 +0200

    Refactored the setup script.

--- src/setuplib.py
+++ src/setuplib.py
@@ -47,7 +47,7 @@ def main(zipped=True):
     scriptDir = os.path.dirname(scriptPath)
     os.chdir(scriptDir)
 
-    print(f'*** Installing {PLUGIN} {VERSION} ***')
+    print(f'*** Installing {PLUGIN} {VERSION} ***\n')
     homePath = str(Path.home()).replace('\\', '/')
     applicationDir = f'{homePath}/.novx'
     if os.path.isdir(applicationDir):
@@ -64,7 +64,7 @@ def main(zipped=True):
 
         # Show a success message.
         print(
-            f'Sucessfully installed "{PLUGIN}" '
+            f'\nSucessfully installed {PLUGIN} '
             f'at "{os.path.normpath(pluginDir)}".'
         )
     else:
